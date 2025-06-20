13ee5aa581f6aac03af895f39591c5210a28e28d
Mathew <mathewkariuki2005@gmail.com>
2025-06-21 01:26:19 +0300

    modified the file

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 function addingEventListener() {
+  const input = document.getElementById('button');
+  function clickAlert() {
+    alert('I was clicked!');
+  }
+  input.addEventListener('click', clickAlert);
 }
+addingEventListener();
+
+
+
+
+// addingEventListener();
+const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
+
+const btn=document.getElementById("button2");
+btn.addEventListener('click',clickAlert);
